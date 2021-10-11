f6935faa8844f034f67e6c346d21564510cdddb5
Filippo Visconti <filippo.visconti23@gmail.com>
2021-10-11 08:24:25 +0200

    Initial check-in of module mcproject

---
+++ mcproject/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using mcproject.ViewModels;
+
+namespace mcproject.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
