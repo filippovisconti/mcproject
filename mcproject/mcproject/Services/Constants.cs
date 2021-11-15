d3d8d6cfd7c1ff2554cb01bcea1c8986871552db
allegra <all.strippoli@stud.uniroma3.it>
2021-11-15 14:18:43 +0100

    added constant collections

--- mcproject/mcproject/Services/Constants.cs
+++ mcproject/mcproject/Services/Constants.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+
 namespace mcproject.Services
 {
     public static class Constants
     {
+        public static ObservableCollection<string> Sport = new ObservableCollection<string>();
+        public static ObservableCollection<string> Livello = new ObservableCollection<string>();
 
+        static Constants()
+        {
+            Sport.Add("Calcetto");
+            Sport.Add("Pallavolo");
+            Sport.Add("Beach volley");
+            Sport.Add("Golf");
+            Livello.Add("Principiante");
+            Livello.Add("Medio");
+            Livello.Add("Avanzato");
+        }
     }
 }
